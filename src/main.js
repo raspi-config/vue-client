f7d37268606a8b3f7b030d51538e9363b72086ab
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2017-11-01 19:39:16 -0200

    Adição do Plugin toastr.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+// import 'toastr/build/toastr.min.css'
 
 import Vue from 'vue'
 import App from './App'
